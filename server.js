b2e23e4d7357998755595d49e8005e66f5520075
Michael C. Main <deusprogrammer@gmail.com>
2020-10-26 01:40:53 -0500

    Added a websocket

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyparser from 'body-parser';
 import cors from 'cors';
 
 const jwt = require('jsonwebtoken');
+const WebSocket = require('ws');
 
 var usersRoutes = require('./api/routes/users');
 var itemRoutes = require('./api/routes/items');
@@ -11,8 +12,26 @@ var jobRoutes = require('./api/routes/jobs');
 var monsterRoutes = require('./api/routes/monsters');
 var abilityRoutes = require('./api/routes/abilities');
 
+// Setup websocket server for communicating with the panel
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.on('connection', (ws) => {
+    let initEvent = {
+        type: "INIT",
+        eventData: {
+            results: {},
+            encounterTable
+        }
+    }
+    ws.send(JSON.stringify(initEvent, null, 5));
+});
+
+wss.on('message', () => {
+    console.log("FART");
+});
+
 let app = express();
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 8081;
 
 // Mongoose instance connection url connection
 const databaseUrl = process.env.CBD_DB_URL;
