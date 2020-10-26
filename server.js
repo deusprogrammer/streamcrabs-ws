26e922c56678adb4dfc195c07dc7c1a5bd01df85
Michael C. Main <deusprogrammer@gmail.com>
2020-10-26 10:42:58 -0500

    Added message routing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
 import cors from 'cors';
+import e from 'express';
 
 const jwt = require('jsonwebtoken');
 const WebSocket = require('ws');
@@ -12,17 +13,55 @@ var jobRoutes = require('./api/routes/jobs');
 var monsterRoutes = require('./api/routes/monsters');
 var abilityRoutes = require('./api/routes/abilities');
 
+// Keys for jwt verification
+const key = process.env.TWITCH_SHARED_SECRET;
+const secret = Buffer.from(key, 'base64');
+
 // Setup websocket server for communicating with the panel
 const wss = new WebSocket.Server({ port: 8082 });
+const clients = {};
 
+// Set up a websocket routing system
 wss.on('connection', (ws) => {
+    console.log("CONNECTION");
     ws.on('message', (message) => {
-        console.log("FART");
-    });
-});
+        let event = JSON.parse(message);
+        console.log("MESSAGE: " + JSON.stringify(event, null, 5));
+        if (event.jwt) {
+            jwt.verify(
+                auth,
+                secret,
+                (err, decoded) => {
+                    if (err) {
+                        console.log('JWT Error', err);
+                        return;
+                    }
 
-wss.on('message', () => {
-    console.log("FART");
+                    event.jwt = null;
+
+                    if (event.type === "REGISTER") {
+                        clients[decoded.user_id] = ws;
+                    } else {
+                        if (event.to === "ALL") {
+                            clients.forEach((to) => {
+                                to.send(JSON.stringify(event));
+                            })
+                        } else {
+                            let to = clients[event.to];
+                            to.send(JSON.stringify(event));
+                        }
+                    }
+                }
+            );
+    
+            return;
+        }
+        clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(message));
+            }
+        });
+    });
 });
 
 let app = express();
@@ -30,8 +69,6 @@ let port = process.env.PORT || 8081;
 
 // Mongoose instance connection url connection
 const databaseUrl = process.env.CBD_DB_URL;
-const key = process.env.TWITCH_SHARED_SECRET;
-const secret = Buffer.from(key, 'base64');
 mongoose.Promise = global.Promise;
 
 /*
