234404f23a6dbd99899144707f58e84d0bbbcba1
Michael C. Main <deusprogrammer@gmail.com>
2020-11-18 12:41:58 -0600

    Updated to correct role locks

--- api/routes/bots.js
+++ api/routes/bots.js
@@ -7,6 +7,11 @@ var Bots = require('../models/bots');
 const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 const BATTLE_BOT_JWT = process.env.BATTLE_BOT_JWT;
+const TWITCH_EXT_CLIENT_ID = process.env.TWITCH_EXT_CLIENT_ID;
+const TWITCH_BOT_CLIENT_ID = process.env.TWITCH_BOT_CLIENT_ID;
+const TWITCH_BOT_USER = process.env.TWITCH_BOT_USER;
+const TWITCH_BOT_PASS = process.env.TWITCH_BOT_PASS;
+const TWITCH_BOT_ACCESS_TOKEN = process.env.TWITCH_BOT_ACCESS_TOKEN;
 const redirectUrl = "https://deusprogrammer.com/util/twitch/registration/callback";
 
 const randomUuid = () => {
@@ -40,6 +45,25 @@ const getProfile = async (accessToken) => {
     }
 }
 
+const createBotContainer = async (userId) => {
+    const url = `http://10.0.0.243:2375/containers/create?name=cbd-bot-${userId}`;
+    let res = await axios.post(url, {
+        Image: "mmain/cbd-bot:latest",
+        Env: [
+            `TWITCH_EXT_CHANNEL_ID=${userId}`,
+            `TWITCH_EXT_CLIENT_ID=${TWITCH_EXT_CLIENT_ID}`,
+            `TWITCH_BOT_ACCESS_TOKEN=${TWITCH_BOT_ACCESS_TOKEN}`,
+            `TWITCH_BOT_USER=${TWITCH_BOT_USER}`,
+            `TWITCH_BOT_PASS=${TWITCH_BOT_PASS}`,
+            `TWITCH_BOT_CLIENT_ID=${TWITCH_BOT_CLIENT_ID}`,
+            "PROFILE_API_URL=https://deusprogrammer.com/api/profile-svc",
+            "BATTLE_API_URL=https://deusprogrammer.com/api/twitch"
+        ]
+    });
+
+    return res.data;
+}
+
 const createTrinaryUser = async (username, userId) => {
     // Create a user for profile service so the main UI page can be accessed
     try {
@@ -55,7 +79,7 @@ const createTrinaryUser = async (username, userId) => {
                     ]
                 }
             },
-            roles: ["SUPER_USER"]
+            roles: []
         }, {
             headers: {
                 contentType: "application/json",
@@ -81,6 +105,9 @@ router.route("/")
             // Create user.
             let profile = userRes.data[0];
             await createTrinaryUser(profile.login, profile.id);
+
+            // Create bot container
+            await createBotContainer(profile.id);
 
             // Create body
             request.body.sharedSecretKey = randomUuid();
